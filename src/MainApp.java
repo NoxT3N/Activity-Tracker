76b89251e724f61d124904de15096bc3150ae473
Felix <145358483+NoxT3N@users.noreply.github.com>
2023-11-07 22:28:28 +0000

    menu method completed with some minor bugs

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,3 +1,5 @@
+import java.lang.invoke.SwitchPoint;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
@@ -8,10 +10,11 @@ public class MainApp {
     public static int minDuration = 60;
     public static ArrayList<Activity> activities;
 
-    public void displayAll(){
+    public static void displayAll(){
         for (Activity a:activities) {
             System.out.println(a.toString());
         }
+        System.out.println();
     }
     public static double avgDistance(ArrayList<Activity> activities, String type) {
 
@@ -41,56 +44,50 @@ public class MainApp {
 
     }
 
-    public static void subTypeViewer(ArrayList<Activity> activities, String type, int subType){
+    public static void printFormatArrayList(){
+        for (Activity act: activities) {
+            printFormatAct(act);
+        }
+    }
+
+    public static void printFormatAct(Activity act){
+        System.out.printf("%-20s %5s %5d %5.2f %5d %n",act.getType(),act.getDate(),act.getDuration(),act.getDistance(),act.getAverageHeartRate());
+    }
+
+
+    public static void subTypeViewerEnergy(String str){
+        String energy = str;
+        if(energy.contains(" ")){
+            energy = str.replace(' ','_');
+        }
         for(Activity act : activities) {
-
-//            if(act.getType().equalsIgnoreCase(type))
-//            {
-//                System.out.printf("%-20s %5s %5d %5.2f %5d %n",type,act.getDate(),act.getDuration(),act.getDistance(),act.getAverageHeartRate());
-//            }
-//            else if(act.getDistance() > minDistance){
-//                System.out.printf("%-20s %5s %5d %5.2f %5d %n",act.getType(),act.getDate(),act.getDuration(),act.getDistance(),act.getAverageHeartRate());
-//            }
-//            else if(act.getDuration() > minDuration){
-//                System.out.printf("%-20s %5s %5d %5.2f %5d %n",act.getType(),act.getDate(),act.getDuration(),act.getDistance(),act.getAverageHeartRate());
-//            }
-
-            switch (subType){
-                case 1:
-                    if(act.getType().equalsIgnoreCase(type))
-                    {
-                        System.out.printf("%-20s %5s %5d %5.2f %5d %n",type,act.getDate(),act.getDuration(),act.getDistance(),act.getAverageHeartRate());
-                    }
-                case 2:
-                    if(act.getDistance() > minDistance){
-                        System.out.printf("%-20s %5s %5d %5.2f %5d %n",act.getType(),act.getDate(),act.getDuration(),act.getDistance(),act.getAverageHeartRate());
-                    }
-                case 3:
-                    if(act.getDuration() > minDuration){
-                        System.out.printf("%-20s %5s %5d %5.2f %5d %n",act.getType(),act.getDate(),act.getDuration(),act.getDistance(),act.getAverageHeartRate());
-                    }
+            if(act.intensity.toString().equalsIgnoreCase(energy)) {
+                printFormatAct(act);
             }
         }
     }
 
-//    public static void aboveDistance(ArrayList<Activity> activities){
-//        for(Activity act : activities){
-//
-//            if(act.getDistance() > minDistance){
-//                System.out.printf("%-20s %5s %5d %5.2f %5d %n",act.getType(),act.getDate(),act.getDuration(),act.getDistance(),act.getAverageHeartRate());
-//            }
-//        }
-//    }
-//
-//    public static void aboveDuration(ArrayList<Activity> activities){
-//        for(Activity act : activities){
-//
-//            if(act.getDuration() > minDuration){
-//                System.out.printf("%-20s %5s %5d %5.2f %5d %n",act.getType(),act.getDate(),act.getDuration(),act.getDistance(),act.getAverageHeartRate());
-//            }
-//        }
-//    }
-
+    public static void subTypeViewer(int subType){
+        for(Activity act : activities) {
+            switch (subType){
+                case 1:
+                    if(act.getDistance() > minDistance){
+                        printFormatAct(act);
+                    }
+                case 2:
+                    if(act.getDuration() > minDuration){
+                        printFormatAct(act);
+                    }
+            }
+        }
+    }
+    public static void subTypeViewer(String type){
+        for(Activity act : activities) {
+            if(act.getType().equalsIgnoreCase(type)) {
+                printFormatAct(act);
+            }
+        }
+    }
 
     public static void sortBy(ArrayList<Activity>activities,String sortRef,boolean asc){
         switch(sortRef.toLowerCase()){
@@ -124,9 +121,11 @@ public class MainApp {
     public static void menu(){
         activityReader reader = new activityReader();
         reader.fileList();
-        ArrayList<Activity> activities = reader.activitiesList;
+        activities = reader.activitiesList;
         int subType = 1;
         boolean exit = false;
+        boolean back = false;
+
         Scanner keyboard = new Scanner(System.in);
 
         System.out.println("Please choose a file number");
@@ -134,43 +133,135 @@ public class MainApp {
             try {
                 reader.userInput = keyboard.nextLine();
                 reader.userChoice();
-                exit = true;
             } catch (Exception e){
                 System.out.println(reader.userInput + " is not a number, please try again.");
             }
+            System.out.println("Please choose what to do with the file:\n");
+            System.out.println("1 - View activity");
+            System.out.println("2 - View a subset of your activity");
+            System.out.println("3 - View statistics");
+            System.out.println("0 - Exit\n");
 
-            System.out.println("Please choose what to do with the file:");
+            int userInput = keyboard.nextInt();
 
+            switch(userInput){
+                case 1:
+                    do{
+                        System.out.println("Sort by: ");
+                        System.out.println("1 - Calories burned");
+                        System.out.println("2 - Date");
+                        System.out.println("3 - Activity Duration");
+                        System.out.println("4 - Type of Activity");
+                        System.out.println("5 - Distance");
+                        System.out.println("0 - Back to Menu\n");
+
+                        userInput = keyboard.nextInt();
+
+                        switch(userInput){
+                            case 1 -> {sortBy(activities,"calories",false); displayAll();}
+                            case 2 ->{
+                                System.out.println("\n1 - Ascending\n2 - Descending");
+                                userInput = keyboard.nextInt();
+                                boolean asc = userInput == 1;
+                                sortBy(activities,"date",asc);
+                                displayAll();
+                            }
+                            case 3 -> {
+                                System.out.println("\n1 - Ascending\n2 - Descending");
+                                userInput = keyboard.nextInt();
+                                boolean asc = userInput == 1;
+                                sortBy(activities,"duration",asc);
+                                displayAll();
+                            }
+                            case 4 -> {sortBy(activities,"type",false); displayAll();}
+                            case 5 -> {
+                                System.out.println("\n1 - Ascending\n2 - Descending");
+                                userInput = keyboard.nextInt();
+                                boolean asc = userInput == 1;
+                                sortBy(activities,"distance",asc);
+                                displayAll();
+                            }
+                            case 0 -> back = true;
+                            default -> System.out.println("Invalid input");
+                        }
+
+                    }while(!back);
+                break;
+                case 2:
+                    do {
+                        System.out.println("1 - Activity type");
+                        System.out.println("2 - Above a minimum distance");
+                        System.out.println("3 - Type of energy expended");
+                        System.out.println("4 - Above a minimum duration");
+                        System.out.println("5 - Activity type");
+                        System.out.println("0 - Back to Menu\n");
+
+                        userInput = keyboard.nextInt();
+
+                        switch (userInput){
+                            case 1 :
+                                System.out.println("1 - Swimming\n2 - Running\n3 - Cycling");
+                                int type = keyboard.nextInt();
+                                switch (type){
+                                    case 1 -> subTypeViewer("Swimming");
+                                    case 2 -> subTypeViewer("Running");
+                                    case 3 -> subTypeViewer("Cycling");
+                                }
+                                break;
+                            case 2:
+                                System.out.println("Enter minimum distance: ");
+                                minDistance = keyboard.nextDouble();
+                                subTypeViewer(1);
+                                break;
+                            case 3:
+                                System.out.println("Enter type of energy");
+                                keyboard.nextLine();
+                                String choice = keyboard.nextLine();
+                                subTypeViewerEnergy(choice);
+                            break;
+                            case 4:
+                                System.out.println("Enter minimum duration: ");
+                                minDuration = keyboard.nextInt();
+                                subTypeViewer(2);
+                            break;
+                            case 0 : back = true;
+                            break;
+                            default:
+                                System.out.println("invalid input");
+                            break;
+                        }
+
+                    }while(!back);
+
+                break;
+                case 3:
+                        System.out.println("1 - Average distance per activity");
+                        System.out.println("2 - Average calories burned");
+                        int choice = keyboard.nextInt();
+                        if (choice == 1) {
+                            String[] types = {"Swimming", "Running", "Cycling"};
+                            String[] pastTypes = {"swam", "ran", "cycled"};
+
+                            for (int i = 0; i < types.length; i++) {
+                                System.out.printf("You have " + pastTypes[i] +" an average distance of %.2f km in this set.\n", avgDistance(activities, types[i]) );
+                            }
+
+                        } else {
+                            System.out.println("You have burned " + avgCalories(activities) + " calories on average in this set.");
+                        }
+                break;
+                case 0:
+                    exit = true;
+                break;
+                default:
+                    System.out.println("Invalid input");
+                break;
+            }
         } while (!exit);
     }
     public static void main(String[] args) {
-
+        System.out.println("Welcome to Activity Tracker");
         menu();
-
-        String[] types = {"Swimming", "Running", "Cycling"};
-        String[] pastTypes = {"swam", "ran", "cycled"};
-//        for(String type: types)
-//        {
-//            System.out.println(type +" " + avgDistance(activities, type));
-//        }
-
-        for (int i = 0; i < types.length; i++) {
-            System.out.printf("You have " + pastTypes[i] +" an average distance of %.2f km in this set.\n", avgDistance(activities, types[i]) );
-        }
-
-        System.out.println("You have burned " + avgCalories(activities) + " calories on average in this set.");
-
-//        subTypeViewer(activities, types[1], 1);
-
-//        aboveDistance(activities);
-//
-//        aboveDuration(activities);
-//        for (Activity a:activities) {
-//            System.out.println(a.toString());
-//        }
-//
-//        System.out.println("\n"+activities.size());
-
     }
 
 }
